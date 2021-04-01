ca33fadb5943b85a37dbb64b3dd4eb518f43b501
Fábio Mallaco Moreira <fabiom91@gmail.com>
2021-04-01 05:56:13 -0300

    Update build script and organization

--- build_frei.sh
+++ build_frei.sh
@@ -1,24 +1,34 @@
-chpl -o frei --main-module "Control" frei.chpl          \
-                                     correction.chpl    \
-                                     polynomials.chpl   \
-                                     interpolation.chpl \
-                                     testing.chpl       \
-                                     input.chpl         \
-                                     mesh.chpl          \
-                                     gmesh.chpl         \
-                                     parameters.chpl    |
-    tee frei_build_dbg.log
+#!/bin/bash
+
+#if test -f "frei_opt-build.log" ; then
+#    mv frei_opt-build.log frei_opt-build.log.old
+#fi
+#chpl -o frei_opt --main-module "FREI" src/FREI.chpl          \
+#                                      src/input.chpl         \
+#                                      src/config.chpl        \
+#                                      src/correction.chpl    \
+#                                      src/polynomials.chpl   \
+#                                      src/interpolation.chpl \
+#                                      src/mesh.chpl          \
+#                                      src/gmesh.chpl         \
+#                                      src/testing.chpl       \
+#                                      src/parameters.chpl    |
+#    tee frei_opt-build.log
 
 
+if test -f "frei_dbg-build.log" ; then
+    mv frei_dbg-build.log frei_dbg-build.log.old
+fi
 
-chpl -o frei --main-module "Control" frei.chpl          \
-                                     correction.chpl    \
-                                     polynomials.chpl   \
-                                     interpolation.chpl \
-                                     testing.chpl       \
-                                     input.chpl         \
-                                     mesh.chpl          \
-                                     gmesh.chpl         \
-                                     parameters.chpl    |
-    tee frei_build_opt.log
+chpl -o frei_dbg --main-module "FREI" src/FREI.chpl          \
+                                      src/input.chpl         \
+                                      src/config.chpl        \
+                                      src/polynomials.chpl   \
+                                      src/correction.chpl    \
+                                      src/interpolation.chpl \
+                                      src/mesh.chpl          \
+                                      src/gmesh.chpl         \
+                                      src/testing.chpl       \
+                                      src/parameters.chpl    |
+    tee frei_dbg-build.log
 
