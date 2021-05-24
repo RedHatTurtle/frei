027fea9a99ba5a7260596623b3119224784427b6
Fábio Mallaco Moreira <fabiom91@gmail.com>
2021-05-24 05:33:42 -0300

    Rename test logs

--- build_tests.sh
+++ build_tests.sh
@@ -3,47 +3,47 @@
 # Build all tests and save log
 echo "Building Flux Tests:"
 chpl -o flux_tests --main-module Flux src/flux.chpl src/input.chpl src/parameters.chpl |
-    tee flux_build_test.log
+    tee flux_build.log
 echo "done"
 echo
 echo "Building Riemann Tests:"
 chpl -o riemann_tests --main-module Riemann src/riemann.chpl src/flux.chpl src/input.chpl src/parameters.chpl |
-    tee riemann_build_test.log
+    tee riemann_build.log
 echo "done"
 echo
 echo "Building Polynomials Tests:"
 chpl -o polynomials_tests --main-module Polynomials src/polynomials.chpl src/testing.chpl src/parameters.chpl |
-    tee polynomials_build_test.log
+    tee polynomials_build.log
 echo "done"
 echo
 echo "Building Interpolation Tests:"
 chpl -o interpolation_tests --main-module Interpolation src/interpolation.chpl src/polynomials.chpl src/parameters.chpl src/testing.chpl |
-    tee interpolation_build_test.log
+    tee interpolation_build.log
 echo "done"
 echo
 echo "Building Correction Tests:"
 chpl -o correction_tests --main-module Correction src/correction.chpl src/polynomials.chpl src/testing.chpl src/input.chpl src/mesh.chpl src/gmesh.chpl src/config.chpl src/parameters.chpl |
-    tee correction_build_test.log
+    tee correction_build.log
 echo "done"
 echo
 echo "Building Gmesh Tests:"
 chpl -o gmesh_tests --main-module Gmesh src/gmesh.chpl src/parameters.chpl |
-    tee gmesh_build_test.log
+    tee gmesh_build.log
 echo "done"
 echo
 echo "Building Mesh Tests:"
 chpl -o mesh_tests --main-module Mesh src/mesh.chpl src/gmesh.chpl src/testing.chpl src/parameters.chpl |
-    tee mesh_build_test.log
+    tee mesh_build.log
 echo "done"
 echo
 echo "Building FRMesh Tests:"
 chpl -o frmesh_tests --main-module FRMesh src/frmesh.chpl src/polynomials.chpl src/mesh.chpl src/gmesh.chpl src/testing.chpl src/config.chpl src/input.chpl src/parameters.chpl |
-    tee frmesh_build_test.log
+    tee frmesh_build.log
 echo "done"
 echo
 echo "Building FR Tests:"
 chpl -o fr_tests --main-module FR src/fr.chpl src/parameters.chpl |
-    tee fr_build_test.log
+    tee fr_build.log
 echo "done"
 echo
 
@@ -52,13 +52,13 @@ echo "Running Tests"
 ./flux_tests          &> flux_tests.log
 ./riemann_tests       &> riemann_tests.log
 
-./polynomials_tests   &> polynomials_test.log
-./interpolation_tests &> interpolation_test.log
-./correction_tests    &> correction_test.log
+./polynomials_tests   &> polynomials_tests.log
+./interpolation_tests &> interpolation_tests.log
+./correction_tests    &> correction_tests.log
 
-./gmesh_tests         &> gmesh_test.log
-./mesh_tests          &> mesh_test.log
-./frmesh_tests        &> frmesh_test.log
+./gmesh_tests         &> gmesh_tests.log
+./mesh_tests          &> mesh_tests.log
+./frmesh_tests        &> frmesh_tests.log
 
-./fr_tests            &> fr_test.log
+./fr_tests            &> fr_tests.log
 echo "Done"
