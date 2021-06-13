19263586c73b906f3dee7afb90ffeee598b8b8a6
Fábio Mallaco Moreira <fabiom91@gmail.com>
2021-06-13 07:24:35 -0300

    Add default BLAS/Lapack locations to FREI build

--- build_frei.sh
+++ build_frei.sh
@@ -4,17 +4,30 @@
 ###   Debug Build                              ###
 ##################################################
 
+PATH_TO_CBLAS_DIR="/usr/include"
+PATH_TO_BLAS_LIBS="/usr/lib64"
+PATH_TO_LAPACKE_INCLUDE_DIR="/usr/include"
+PATH_TO_LIBGFORTRAN="/usr/lib64"
+PATH_TO_LAPACK_BINARIES="/usr/lib64"
+
 if test -f "frei_dbg-build.log" ; then
     mv frei_dbg-build.log frei_dbg-build.log.old
 fi
 
-chpl --set lapackImpl=off                        \
-     --set blasImpl=off                          \
+#    --set lapackImpl=off                        \
+#    --set blasImpl=off                          \
+
+chpl -o frei_dbg                                 \
      --warnings                                  \
      --warn-unstable                             \
-     -o frei_dbg                                 \
+     -I/usr/include                              \
+     -L/usr/lib64 -lcblas                        \
+     -I/usr/include                              \
+     -L/usr/lib64 -lgfortran                     \
+     -L/usr/lib64 -llapacke -llapack -lcblas     \
      --main-module "FREI" src/FREI.chpl          \
                           src/fr.chpl            \
+                          src/temporal.chpl      \
                           src/output.chpl        \
                           src/boundary.chpl      \
                           src/frmesh.chpl        \
@@ -34,18 +47,28 @@ chpl --set lapackImpl=off                        \
     tee frei_dbg-build.log
 
 ##################################################
-###   Optimized / Prduction Build              ###
+###   Optimized / Production Build             ###
 ##################################################
+
+PATH_TO_CBLAS_DIR="/usr/include"
+PATH_TO_BLAS_LIBS="/usr/lib64"
+PATH_TO_LAPACKE_INCLUDE_DIR="/usr/include"
+PATH_TO_LIBGFORTRAN="/usr/lib64"
+PATH_TO_LAPACK_BINARIES="/usr/lib64"
 
 if test -f "frei_opt-build.log" ; then
     mv frei_opt-build.log frei_opt-build.log.old
 fi
-chpl --set lapackImpl=off                        \
-     --set blasImpl=off                          \
+chpl -o frei_opt                                 \
      --fast                                      \
-     -o frei_opt                                 \
+     -I$PATH_TO_CBLAS_DIR                        \
+     -L$PATH_TO_BLAS_LIBS -lcblas                \
+     -I$PATH_TO_LAPACKE_INCLUDE_DIR              \
+     -L$PATH_TO_LIBGFORTRAN -lgfortran           \
+     -L$PATH_TO_LAPACK_BINARIES -llapacke -llapack -lcblas     \
      --main-module "FREI" src/FREI.chpl          \
                           src/fr.chpl            \
+                          src/temporal.chpl      \
                           src/output.chpl        \
                           src/boundary.chpl      \
                           src/frmesh.chpl        \
@@ -63,3 +86,46 @@ chpl --set lapackImpl=off                        \
                           src/parameters.chpl    \
                           src/testing.chpl       |
     tee frei_opt-build.log
+
+##################################################
+###   Optimized / Production Build             ###
+##################################################
+
+PATH_TO_CBLAS_DIR="/opt/intel/oneapi/mkl/latest/include"
+PATH_TO_BLAS_LIBS="/opt/intel/oneapi/mkl/latest/lib/intel64"
+PATH_TO_LAPACKE_INCLUDE_DIR="/opt/intel/oneapi/mkl/latest/include"
+PATH_TO_LIBGFORTRAN="/opt/intel/oneapi/mkl/latest/lib/intel64"
+PATH_TO_LAPACK_BINARIES="/opt/intel/oneapi/mkl/latest/lib/intel64"
+
+#if test -f "frei_opt_mkl-build.log" ; then
+#    mv frei_opt_mkl-build.log frei_opt_mkl-build.log.old
+#fi
+#chpl -o frei_opt_mkl                             \
+#     --fast                                      \
+#     -I$PATH_TO_CBLAS_DIR                        \
+#     -L$PATH_TO_BLAS_LIBS -lblas                 \
+#     -I$PATH_TO_LAPACKE_INCLUDE_DIR              \
+#     -L$PATH_TO_LIBGFORTRAN -lgfortran           \
+#     -L$PATH_TO_LAPACK_BINARIES -llapacke -llapack -lrefblas \
+#     --set blasImpl=mkl                          \
+#     --set lapackImpl=mkl                        \
+#     --main-module "FREI" src/FREI.chpl          \
+#                          src/fr.chpl            \
+#                          src/temporal.chpl      \
+#                          src/output.chpl        \
+#                          src/boundary.chpl      \
+#                          src/frmesh.chpl        \
+#                          src/mesh.chpl          \
+#                          src/gmesh.chpl         \
+#                          src/riemann.chpl       \
+#                          src/flux.chpl          \
+#                          src/correction.chpl    \
+#                          src/interpolation.chpl \
+#                          src/polynomials.chpl   \
+#                          src/sourceterm.chpl    \
+#                          src/init.chpl          \
+#                          src/config.chpl        \
+#                          src/input.chpl         \
+#                          src/parameters.chpl    \
+#                          src/testing.chpl       |
+#    tee frei_opt-build.log
