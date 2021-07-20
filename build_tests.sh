3cd1b7af4d2ef1c1c8404be1958400bdb5bb172a
Fábio Malacco Moreira <fabiom91@gmail.com>
2021-07-20 09:21:16 -0300

    Add warnings to tests compilation

--- build_tests.sh
+++ build_tests.sh
@@ -2,57 +2,90 @@
 
 # Build all tests and save log
 echo "Building Polynomials Tests:"
-chpl -o polynomials_tests --main-module Polynomials src/polynomials.chpl src/testing.chpl src/parameters.chpl |
+chpl -o polynomials_tests                          \
+     --warnings                                    \
+     --warn-unstable                               \
+     --main-module Polynomials src/polynomials.chpl src/testing.chpl src/parameters.chpl |
     tee polynomials_build.log
 echo "done"
 echo
 echo "Building Interpolation Tests:"
-chpl -o interpolation_tests --main-module Interpolation src/interpolation.chpl src/polynomials.chpl src/parameters.chpl src/testing.chpl |
+chpl -o interpolation_tests                        \
+     --warnings                                    \
+     --warn-unstable                               \
+     --main-module Interpolation src/interpolation.chpl src/polynomials.chpl src/parameters.chpl src/testing.chpl |
     tee interpolation_build.log
 echo "done"
 echo
 echo "Building Correction Tests:"
-chpl -o correction_tests --main-module Correction src/correction.chpl src/polynomials.chpl src/testing.chpl src/input.chpl src/mesh.chpl src/gmesh.chpl src/config.chpl src/parameters.chpl |
+chpl -o correction_tests                           \
+     --warnings                                    \
+     --warn-unstable                               \
+     --main-module Correction src/correction.chpl src/polynomials.chpl src/testing.chpl src/input.chpl src/mesh.chpl src/gmesh.chpl src/config.chpl src/parameters.chpl |
     tee correction_build.log
 echo "done"
 echo
 echo "Building Gmesh Tests:"
-chpl -o gmesh_tests --main-module Gmesh src/gmesh.chpl src/parameters.chpl |
+chpl -o gmesh_tests                                \
+     --warnings                                    \
+     --warn-unstable                               \
+     --main-module Gmesh src/gmesh.chpl src/parameters.chpl |
     tee gmesh_build.log
 echo "done"
 echo
 echo "Building Mesh Tests:"
-chpl -o mesh_tests --main-module Mesh src/mesh.chpl src/gmesh.chpl src/testing.chpl src/parameters.chpl |
+chpl -o mesh_tests                                 \
+     --warnings                                    \
+     --warn-unstable                               \
+     --main-module Mesh src/mesh.chpl src/gmesh.chpl src/testing.chpl src/parameters.chpl |
     tee mesh_build.log
 echo "done"
 echo
 echo "Building FRMesh Tests:"
-chpl -o frmesh_tests --main-module FRMesh src/frmesh.chpl src/polynomials.chpl src/mesh.chpl src/gmesh.chpl src/testing.chpl src/config.chpl src/input.chpl src/parameters.chpl |
+chpl -o frmesh_tests                               \
+     --warnings                                    \
+     --warn-unstable                               \
+     --main-module FRMesh src/frmesh.chpl src/polynomials.chpl src/mesh.chpl src/gmesh.chpl src/testing.chpl src/config.chpl src/input.chpl src/parameters.chpl |
     tee frmesh_build.log
 echo "done"
 echo
 echo "Building Flux Tests:"
-chpl -o flux_tests --main-module Flux src/flux.chpl src/input.chpl src/mesh.chpl src/gmesh.chpl src/parameters.chpl |
+chpl -o flux_tests                                 \
+     --warnings                                    \
+     --warn-unstable                               \
+     --main-module Flux src/flux.chpl src/input.chpl src/mesh.chpl src/gmesh.chpl src/parameters.chpl |
     tee flux_build.log
 echo "done"
 echo
 echo "Building Riemann Tests:"
-chpl -o riemann_tests --main-module Riemann src/riemann.chpl src/flux.chpl src/input.chpl src/mesh.chpl src/gmesh.chpl src/parameters.chpl |
+chpl -o riemann_tests                              \
+     --warnings                                    \
+     --warn-unstable                               \
+     --main-module Riemann src/riemann.chpl src/flux.chpl src/input.chpl src/mesh.chpl src/gmesh.chpl src/parameters.chpl |
     tee riemann_build.log
 echo "done"
 echo
 echo "Building Init Tests:"
-chpl -o init_tests --main-module Init src/init.chpl src/flux.chpl src/config.chpl src/input.chpl src/mesh.chpl src/gmesh.chpl src/parameters.chpl |
+chpl -o init_tests                                 \
+     --warnings                                    \
+     --warn-unstable                               \
+     --main-module Init src/init.chpl src/flux.chpl src/config.chpl src/input.chpl src/mesh.chpl src/gmesh.chpl src/parameters.chpl |
     tee init_build.log
 echo "done"
 echo
 echo "Building Boundary Tests:"
-chpl -o fr_tests --main-module Boundary src/boundary.chpl src/init.chpl src/flux.chpl src/mesh.chpl src/gmesh.chpl src/config.chpl src/input.chpl src/parameters.chpl |
+chpl -o fr_tests                                   \
+     --warnings                                    \
+     --warn-unstable                               \
+     --main-module Boundary src/boundary.chpl src/init.chpl src/flux.chpl src/mesh.chpl src/gmesh.chpl src/config.chpl src/input.chpl src/parameters.chpl |
     tee boundary_build.log
 echo "done"
 echo
 echo "Building FR Tests:"
-chpl -o fr_tests --main-module FR src/fr.chpl |
+chpl -o fr_tests                                   \
+     --warnings                                    \
+     --warn-unstable                               \
+     --main-module FR src/fr.chpl |
     tee fr_build.log
 echo "done"
 echo
