a374a08e427e9d7775144529345fabe5df3f17b1
Fábio Malacco Moreira <fabiom91@gmail.com>
2021-08-19 08:53:49 -0300

    Move tests and build to separate folders

--- build_frei.sh
+++ build_frei.sh
@@ -10,14 +10,14 @@ PATH_TO_LAPACKE_INCLUDE_DIR="/usr/include"
 PATH_TO_LIBGFORTRAN="/usr/lib64"
 PATH_TO_LAPACK_BINARIES="/usr/lib64"
 
-if test -f "frei_dbg-build.log" ; then
-    mv frei_dbg-build.log frei_dbg-build.log.old
+if test -f "build/frei_dbg-build.log" ; then
+    mv build/frei_dbg-build.log build/frei_dbg-build.log.old
 fi
 
 #    --set lapackImpl=off                        \
 #    --set blasImpl=off                          \
 
-chpl -o frei_dbg                                 \
+chpl -o build/frei_dbg                           \
      --warnings                                  \
      --warn-unstable                             \
      -I/usr/include                              \
@@ -47,7 +47,7 @@ chpl -o frei_dbg                                 \
                           src/input.chpl         \
                           src/parameters.chpl    \
                           src/testing.chpl       |
-    tee frei_dbg-build.log
+    tee build/frei_dbg-build.log
 
 ##################################################
 ###   Optimized / Production Build             ###
@@ -59,10 +59,10 @@ PATH_TO_LAPACKE_INCLUDE_DIR="/usr/include"
 PATH_TO_LIBGFORTRAN="/usr/lib64"
 PATH_TO_LAPACK_BINARIES="/usr/lib64"
 
-if test -f "frei_opt-build.log" ; then
-    mv frei_opt-build.log frei_opt-build.log.old
+if test -f "build/frei_opt-build.log" ; then
+    mv build/frei_opt-build.log build/frei_opt-build.log.old
 fi
-chpl -o frei_opt                                 \
+chpl -o build/frei_opt                           \
      --fast                                      \
      -I$PATH_TO_CBLAS_DIR                        \
      -L$PATH_TO_BLAS_LIBS -lcblas                \
@@ -91,7 +91,7 @@ chpl -o frei_opt                                 \
                           src/input.chpl         \
                           src/parameters.chpl    \
                           src/testing.chpl       |
-    tee frei_opt-build.log
+    tee build/frei_opt-build.log
 
 ##################################################
 ###   Optimized / Production Build             ###
@@ -106,7 +106,7 @@ PATH_TO_LAPACK_BINARIES="/opt/intel/oneapi/mkl/latest/lib/intel64"
 #if test -f "frei_opt_mkl-build.log" ; then
 #    mv frei_opt_mkl-build.log frei_opt_mkl-build.log.old
 #fi
-#chpl -o frei_opt_mkl                             \
+#chpl -o build/frei_opt_mkl                       \
 #     --fast                                      \
 #     -I$PATH_TO_CBLAS_DIR                        \
 #     -L$PATH_TO_BLAS_LIBS -lblas                 \
@@ -134,4 +134,4 @@ PATH_TO_LAPACK_BINARIES="/opt/intel/oneapi/mkl/latest/lib/intel64"
 #                          src/input.chpl         \
 #                          src/parameters.chpl    \
 #                          src/testing.chpl       |
-#    tee frei_opt-build.log
+#    tee build/frei_opt-build.log
--- build_tests.sh
+++ build_tests.sh
@@ -8,31 +8,31 @@ PATH_TO_LAPACK_BINARIES="/usr/lib64"
 
 # Build all tests and save log
 echo "Building Polynomials Tests:"
-chpl -o polynomials_tests                          \
+chpl -o tests/polynomials_tests                          \
      --warnings                                    \
      --warn-unstable                               \
      --main-module Polynomials src/polynomials.chpl src/testing.chpl src/parameters.chpl |
-    tee polynomials_build.log
+    tee tests/polynomials_build.log
 echo "done"
 echo
 echo "Building Quadrature Tests:"
-chpl -o quadrature_tests                           \
+chpl -o tests/quadrature_tests                           \
      --warnings                                    \
      --warn-unstable                               \
      --main-module Quadrature src/quadrature.chpl src/polynomials.chpl src/testing.chpl src/parameters.chpl |
-    tee quadrature_build.log
+    tee tests/quadrature_build.log
 echo "done"
 echo
 echo "Building Interpolation Tests:"
-chpl -o interpolation_tests                        \
+chpl -o tests/interpolation_tests                        \
      --warnings                                    \
      --warn-unstable                               \
      --main-module Interpolation src/interpolation.chpl src/polynomials.chpl src/parameters.chpl src/testing.chpl |
-    tee interpolation_build.log
+    tee tests/interpolation_build.log
 echo "done"
 echo
 echo "Building Projection Tests:"
-chpl -o projection_tests                           \
+chpl -o tests/projection_tests                           \
      --warnings                                    \
      --warn-unstable                               \
      -I/usr/include                                \
@@ -45,75 +45,75 @@ chpl -o projection_tests                           \
                               src/polynomials.chpl \
                               src/testing.chpl     \
                               src/parameters.chpl  |
-    tee projection_build.log
+    tee tests/projection_build.log
 echo "done"
 echo
 echo "Building Correction Tests:"
-chpl -o correction_tests                           \
+chpl -o tests/correction_tests                           \
      --warnings                                    \
      --warn-unstable                               \
      --main-module Correction src/correction.chpl src/polynomials.chpl src/testing.chpl src/input.chpl src/mesh.chpl src/gmesh.chpl src/config.chpl src/parameters.chpl |
-    tee correction_build.log
+    tee tests/correction_build.log
 echo "done"
 echo
 echo "Building Gmesh Tests:"
-chpl -o gmesh_tests                                \
+chpl -o tests/gmesh_tests                                \
      --warnings                                    \
      --warn-unstable                               \
      --main-module Gmesh src/gmesh.chpl src/parameters.chpl |
-    tee gmesh_build.log
+    tee tests/gmesh_build.log
 echo "done"
 echo
 echo "Building Mesh Tests:"
-chpl -o mesh_tests                                 \
+chpl -o tests/mesh_tests                                 \
      --warnings                                    \
      --warn-unstable                               \
      --main-module Mesh src/mesh.chpl src/gmesh.chpl src/testing.chpl src/parameters.chpl |
-    tee mesh_build.log
+    tee tests/mesh_build.log
 echo "done"
 echo
 echo "Building FRMesh Tests:"
-chpl -o frmesh_tests                               \
+chpl -o tests/frmesh_tests                         \
      --warnings                                    \
      --warn-unstable                               \
      --main-module FRMesh src/frmesh.chpl src/polynomials.chpl src/mesh.chpl src/gmesh.chpl src/testing.chpl src/config.chpl src/input.chpl src/parameters.chpl |
-    tee frmesh_build.log
+    tee tests/frmesh_build.log
 echo "done"
 echo
 echo "Building Flux Tests:"
-chpl -o flux_tests                                 \
+chpl -o tests/flux_tests                                 \
      --warnings                                    \
      --warn-unstable                               \
      --main-module Flux src/flux.chpl src/input.chpl src/mesh.chpl src/gmesh.chpl src/parameters.chpl |
-    tee flux_build.log
+    tee tests/flux_build.log
 echo "done"
 echo
 echo "Building Riemann Tests:"
-chpl -o riemann_tests                              \
+chpl -o tests/riemann_tests                              \
      --warnings                                    \
      --warn-unstable                               \
      --main-module Riemann src/riemann.chpl src/flux.chpl src/input.chpl src/mesh.chpl src/gmesh.chpl src/parameters.chpl |
-    tee riemann_build.log
+    tee tests/riemann_build.log
 echo "done"
 echo
 echo "Building Init Tests:"
-chpl -o init_tests                                 \
+chpl -o tests/init_tests                                 \
      --warnings                                    \
      --warn-unstable                               \
      --main-module Init src/init.chpl src/flux.chpl src/config.chpl src/input.chpl src/mesh.chpl src/gmesh.chpl src/parameters.chpl |
-    tee init_build.log
+    tee tests/init_build.log
 echo "done"
 echo
 echo "Building Boundary Tests:"
-chpl -o boundary_tests                             \
+chpl -o tests/boundary_tests                             \
      --warnings                                    \
      --warn-unstable                               \
      --main-module Boundary src/boundary.chpl src/init.chpl src/flux.chpl src/mesh.chpl src/gmesh.chpl src/config.chpl src/input.chpl src/parameters.chpl |
-    tee boundary_build.log
+    tee tests/boundary_build.log
 echo "done"
 echo
 echo "Building Limiter Tests:"
-chpl -o limiter_tests                              \
+chpl -o tests/limiter_tests                              \
      --warnings                                    \
      --warn-unstable                               \
      -I/usr/include                                \
@@ -122,37 +122,37 @@ chpl -o limiter_tests                              \
      -L/usr/lib64 -lgfortran                       \
      -L/usr/lib64 -llapacke -llapack -lcblas       \
      --main-module Limiter src/limiter.chpl src/projection.chpl src/quadrature.chpl src/polynomials.chpl src/parameters.chpl src/testing.chpl |
-    tee limiter_build.log
+    tee tests/limiter_build.log
 echo "done"
 echo
 echo "Building FR Tests:"
-chpl -o fr_tests                                   \
+chpl -o tests/fr_tests                                   \
      --warnings                                    \
      --warn-unstable                               \
      --main-module FR src/fr.chpl |
-    tee fr_build.log
+    tee tests/fr_build.log
 echo "done"
 echo
 
 echo "Running Tests"
 # Run tests and output to file
-./polynomials_tests   &> polynomials_tests.log
-./quadrature_tests    &> quadrature_tests.log
-./interpolation_tests &> interpolation_tests.log
-./projection_tests    &> projection_tests.log
+./tests/polynomials_tests   &> tests/polynomials_tests.log
+./tests/quadrature_tests    &> tests/quadrature_tests.log
+./tests/interpolation_tests &> tests/interpolation_tests.log
+./tests/projection_tests    &> tests/projection_tests.log
 
-./correction_tests    &> correction_tests.log
+./tests/correction_tests    &> tests/correction_tests.log
 
-./gmesh_tests         &> gmesh_tests.log
-./mesh_tests          &> mesh_tests.log
-./frmesh_tests        &> frmesh_tests.log
+./tests/gmesh_tests         &> tests/gmesh_tests.log
+./tests/mesh_tests          &> tests/mesh_tests.log
+./tests/frmesh_tests        &> tests/frmesh_tests.log
 
-./flux_tests          &> flux_tests.log
-./riemann_tests       &> riemann_tests.log
+./tests/flux_tests          &> tests/flux_tests.log
+./tests/riemann_tests       &> tests/riemann_tests.log
 
-./init_tests          &> init_tests.log
-./boundary_tests      &> boundary_tests.log
-./limiter_tests       &> limiter_tests.log
+./tests/init_tests          &> tests/init_tests.log
+./tests/boundary_tests      &> tests/boundary_tests.log
+./tests/limiter_tests       &> tests/limiter_tests.log
 
-./fr_tests            &> fr_tests.log
+./tests/fr_tests            &> tests/fr_tests.log
 echo "Done"
