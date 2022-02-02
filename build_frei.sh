5f61ef6c2e8d3936ca4acf96a1686980d8051251
Fábio Malacco Moreira <fabiom91@gmail.com>
2022-02-01 23:04:20 -0300

    Upgrage FREI build script

--- build_frei.sh
+++ build_frei.sh
@@ -1,35 +1,61 @@
 #!/bin/bash
 
-echo
-echo "------------------------------------------------------------"
+FREI_DIR=$(pwd)
+GIT_HASH=$(git rev-parse HEAD)
+GIT_HASH_SHORT=$(git rev-parse --short HEAD)
+COMPILE_TIME=$(date -u +'%Y-%m-%d %H:%M:%S UTC')
+GIT_BRANCH=$(git branch | grep "^\*" | sed 's/^..//')
+SOURCES_HASH=$(sha1sum ./src/*.chpl | sha1sum | head -c 40)
+BUILD_SCRIPT_HASH=$(sha1sum ./build_frei.sh | head -c 40)
+CHANGES=``
+
+git diff --quiet HEAD ./src; SOURCE_CHANGES=$?
+git diff --quiet HEAD ./build_frei.sh; BUILD_SCRIPT_CHANGES=$?
+if [ $SOURCE_CHANGES -eq 0 ] && [ $BUILD_SCRIPT_CHANGES -eq 0 ] ; then
+    echo "Compiling FREI"
+    echo "   - Commit:" $GIT_HASH
+    echo "   - Branch:" $GIT_BRANCH
+    echo "   - Time  :" $COMPILE_TIME
+elif [ $SOURCE_CHANGES -eq 0 ] && [ $BUILD_SCRIPT_CHANGES -ne 0 ] ; then
+    CHANGES=$CHANGES".bld-${BUILD_SCRIPT_HASH::6}"
+    echo "Compiling FREI with modified build script"
+    echo "   - Commit:" $GIT_HASH
+    echo "   - Branch:" $GIT_BRANCH
+    echo "   - Time  :" $COMPILE_TIME
+    echo "Build script hash:" $BUILD_SCRIPT_HASH
+else
+    CHANGES=$CHANGES".src-${SOURCES_HASH::6}"
+    if [ $BUILD_SCRIPT_CHANGES -ne 0 ] ; then
+        CHANGES=$CHANGES".bld-${BUILD_SCRIPT_HASH::6}"
+    fi
+    echo "Compiling FREI with modified sources based on"
+    echo "   - Commit:" $GIT_HASH
+    echo "   - Branch:" $GIT_BRANCH
+    echo "   - Time  :" $COMPILE_TIME
+    echo "Build script hash:" $BUILD_SCRIPT_HASH
+    echo "Source files hash:" $SOURCES_HASH
+fi
+
+EXTENSION="$GIT_HASH_SHORT$CHANGES"
 
 if [ ! -d "build" ]; then
-    echo
     echo "Creating build directory"
     mkdir build
-    echo
-    echo "------------------------------------------------------------"
 fi
+echo "------------------------------------------------------------"
 
 ##################################################
 ###   Debug Build                              ###
 ##################################################
-
-echo
-echo "Building debug version of Frei..."
-echo
-
 PATH_TO_CBLAS_DIR="/usr/include"
 PATH_TO_BLAS_LIBS="/usr/lib64"
 PATH_TO_LAPACKE_INCLUDE_DIR="/usr/include"
 PATH_TO_LIBGFORTRAN="/usr/lib64"
 PATH_TO_LAPACK_BINARIES="/usr/lib64"
 
-if test -f "build/frei_dbg-build.log" ; then
-    mv build/frei_dbg-build.log build/frei_dbg-build.log.old
-fi
-
-chpl -o build/frei_dbg                                     \
+echo "(1/3) Building Debug version of Frei..."
+echo
+chpl -o build/frei_dbg.$EXTENSION                          \
      --warnings                                            \
      --warn-unstable                                       \
      -I$PATH_TO_CBLAS_DIR                                  \
@@ -61,34 +87,26 @@ chpl -o build/frei_dbg                                     \
                           src/input.chpl                   \
                           src/parameters.chpl              \
                           src/testing.chpl                 \
-    2>&1 | tee build/frei_dbg-build.log
-if [ $? -eq 0 ]; then
-  echo -e "\nSuccess"
+    2>&1 | tee build/frei_dbg.$EXTENSION.log
+if [ ${PIPESTATUS[0]} -eq 0 ]; then
+    echo "OK"
+    ln -srf build/frei_dbg.$EXTENSION ./build/frei_dbg
+    if [ $SOURCE_CHANGES -eq 0 ] && [ $BUILD_SCRIPT_CHANGES -eq 0 ] ; then
+        ln -srf build/frei_dbg.$EXTENSION ./build/frei_dbg.$GIT_BRANCH
+    else
+        ln -srf build/frei_dbg.$EXTENSION ./build/frei_dbg.$GIT_BRANCH-alpha
+    fi
 else
-  echo -e "\nFailed"
+    echo "FAILED"
 fi
-echo
 echo "------------------------------------------------------------"
 
 ##################################################
 ###   Optimized / Production Build             ###
 ##################################################
-
+echo "(2/3) Building Optimized version of Frei..."
 echo
-echo "Building Optimized version of Frei..."
-echo
-
-PATH_TO_CBLAS_DIR="/usr/include"
-PATH_TO_BLAS_LIBS="/usr/lib64"
-PATH_TO_LAPACKE_INCLUDE_DIR="/usr/include"
-PATH_TO_LIBGFORTRAN="/usr/lib64"
-PATH_TO_LAPACK_BINARIES="/usr/lib64"
-
-if test -f "build/frei_opt-build.log" ; then
-    mv build/frei_opt-build.log build/frei_opt-build.log.old
-fi
-
-chpl -o build/frei_opt                                     \
+chpl -o build/frei_opt.$EXTENSION                          \
      --fast                                                \
      -I$PATH_TO_CBLAS_DIR                                  \
      -L$PATH_TO_BLAS_LIBS -lcblas                          \
@@ -119,34 +137,32 @@ chpl -o build/frei_opt                                     \
                           src/input.chpl                   \
                           src/parameters.chpl              \
                           src/testing.chpl                 \
-    2>&1 | tee build/frei_opt-build.log
-if [ $? -eq 0 ]; then
-  echo -e "\nSuccess"
+    2>&1 | tee build/frei_opt.$EXTENSION.log
+if [ ${PIPESTATUS[0]} -eq 0 ]; then
+    echo "OK"
+    ln -srf build/frei_opt.$EXTENSION ./build/frei_opt
+    if [ $SOURCE_CHANGES -eq 0 ] && [ $BUILD_SCRIPT_CHANGES -eq 0 ] ; then
+        ln -srf build/frei_opt.$EXTENSION ./build/frei_opt.$GIT_BRANCH
+    else
+        ln -srf build/frei_opt.$EXTENSION ./build/frei_opt.$GIT_BRANCH-alpha
+    fi
 else
-  echo -e "\nFailed"
+    echo "FAILED"
 fi
-echo
 echo "------------------------------------------------------------"
 
 ##################################################
-###   Optimized / Production Build             ###
+###   Optimizes Intel MKL Build                ###
 ##################################################
-
-echo
-echo "Building Optimized Intel MKL version of Frei..."
-echo
-
 PATH_TO_CBLAS_DIR="/opt/intel/oneapi/mkl/latest/include"
 PATH_TO_BLAS_LIBS="/opt/intel/oneapi/mkl/latest/lib/intel64"
 PATH_TO_LAPACKE_INCLUDE_DIR="/opt/intel/oneapi/mkl/latest/include"
 PATH_TO_LIBGFORTRAN="/opt/intel/oneapi/mkl/latest/lib/intel64"
 PATH_TO_LAPACK_BINARIES="/opt/intel/oneapi/mkl/latest/lib/intel64"
 
-if test -f "frei_opt_mkl-build.log" ; then
-    mv frei_opt_mkl-build.log frei_opt_mkl-build.log.old
-fi
-
-chpl -o build/frei_opt_mkl                                 \
+echo "(3/3) Building Optimized Intel MKL version of Frei..."
+echo
+chpl -o build/frei_mkl.$EXTENSION                          \
      --fast                                                \
      --set blasImpl=mkl                                    \
      --set lapackImpl=mkl                                  \
@@ -179,11 +195,16 @@ chpl -o build/frei_opt_mkl                                 \
                           src/input.chpl                   \
                           src/parameters.chpl              \
                           src/testing.chpl                 \
-    2>&1 | tee build/frei_opt-build.log
-if [ $? -eq 0 ]; then
-  echo -e "\nSuccess"
+    2>&1 | tee build/frei_mkl.$EXTENSION.log
+if [ ${PIPESTATUS[0]} -eq 0 ]; then
+    echo "OK"
+    ln -srf build/frei_mkl.$EXTENSION ./build/frei_mkl
+    if [ $SOURCE_CHANGES -eq 0 ] && [ $BUILD_SCRIPT_CHANGES -eq 0 ] ; then
+        ln -srf build/frei_mkl.$EXTENSION ./build/frei_mkl.$GIT_BRANCH
+    else
+        ln -srf build/frei_mkl.$EXTENSION ./build/frei_mkl.$GIT_BRANCH-alpha
+    fi
 else
-  echo -e "\nFailed"
+    echo "FAILED"
 fi
-echo
 echo "------------------------------------------------------------"
